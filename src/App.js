c2f00a36ab60211914c6afb7d67aa3e25b8dedf1
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-22 08:03:29 -0600

    built input with redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import InputMirror from './components/InputMirror'
+import store from './store/index'
 
 class App extends Component {
   render() {
     return (
         <div className="App">
+          <InputMirror store={ store }/>
         </div>
     );
   }
