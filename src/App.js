4db645844996813f0811af37390fe42c76392681
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-22 07:23:56 -0600

    built again with increment and decrement buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Counter from './components/Counter'
+import store from './store/index'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+        <div className="App">
+            <Counter store={store}/>
+        </div>
     );
   }
 }
