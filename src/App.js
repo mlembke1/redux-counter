d96dddb2633cd038c324863a42c3435fd9ab17b4
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-22 07:28:41 -0600

    burned it all down again. now building an input field

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from './components/Counter'
-import store from './store/index'
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-            <Counter store={store}/>
         </div>
     );
   }
